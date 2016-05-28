8e8a04eab7720148b9e048be9d43a968635e904d
stivehu <stive.hu@gmail.com>
2016-05-28 17:23:51 +0200

    Clone with event

--- assets/js/html.sortable.dblclick.js
+++ assets/js/html.sortable.dblclick.js
@@ -1,21 +1,19 @@
-sortable.dblclick = function() {  
-var panelId=[];
-$(".kv-connected").each(
-        function(){
-            panelId.push($(this).attr('id'));            
-        });
-var swapItem=function (source,id,id2){
-        var litem = $(source).clone();
-    result=litem.appendTo($('#'+id));    
-    $(source).remove();
-    result.dblclick(function() {
-        swapItem(this,id2);
-  });
-}
-  $('#'+panelId[0]+' li').dblclick(function() {
-        swapItem(this,panelId[1],panelId[0]);
-  });
-  $('#'+panelId[1]+' li').dblclick(function() {
-        swapItem(this,panelId[0],panelId[1]);
-  });    
-  };
+sortable.dblclick = function () {
+    var panelId = [];
+    $(".kv-connected").each(
+            function () {
+                panelId.push($(this).attr('id'));
+            });
+    var swapItem = function (source, id, id2) {
+        var litem = $(source).clone(true);
+        result = litem.appendTo($('#' + id));
+        $(source).remove();
+    }
+    $('#' + panelId[0] + ' li,' + '#' + panelId[1] + ' li').dblclick(function () {
+        if ($(this).parent().attr('id') == panelId[0]) {
+            swapItem(this, panelId[1], panelId[0]);
+        } else {
+            swapItem(this, panelId[0], panelId[1]);
+        }
+    });
+};
